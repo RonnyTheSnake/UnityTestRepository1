6e4d66c7a5e10234861172c5f20ab66e0789a7ae
RonnyTheSnake <angelodarwiche.arbete@gmail.com>
2023-01-19 16:05:01 +0100

    Jump

--- My project/Assets/Player.cs
+++ My project/Assets/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] int speed = 5;
+    [SerializeField] float jump = 0;
+    [SerializeField] float gravity = 0.5f;
+    [SerializeField] float jumpHeight = 25f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 input = new Vector3(Input.)
+        //Vector3 input = new Vector3();
+        //transform.position += new Vector3(0, 5, 0) * Time.deltaTime;
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * 2, jump, Input.GetAxis("Vertical") * 2) * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump += jumpHeight;
+        }
+        if(jump > 0)
+        {
+            jump -= gravity;
+        }
+        if( jump < 0)
+        {
+            jump = 0;
+        }
     }
 }
